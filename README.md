3ccbb34fb069a97d3ed7706e1b20ff4968feba34
kevin <kevin>
2013-01-05 22:32:51 -0500

    init
    First commit

---
+++ README.md
@@ -0,0 +1,77 @@
+# cvBlob
+
+**Maintainer:** Kevin Walchko
+
+**License:** LGPL
+
+cvBlob is written for OpenCV 2.x library which uses modern C++ concepts. 
+
+cvBlob is cloned from the original work of Cristóbal Carnero Liñán (grendel.ccl@gmail.com)
+on his [cvBlob](http://code.google.com/p/cvblob/). 
+
+---------------------------------------------------------------------------------------
+
+## To Do
+
+* Change all `structs` to `classes`
+* Change `IplImage` to `Mat`
+* Optimize code
+* Document code for doxygen output
+* Build and install documentation to share/docs
+* Make better name than cvBlob so as not to add more confusion with all of the cvBlob libraries
+
+## Source Code
+
+The git repository is located at [cvBlob](http://github.com/walcko/cvBlob). The code can be checked out:
+
+	git clone http://github.com/walcko/cvBlob
+
+## Compiling and Installation
+
+### Linux
+
+You will need to install openvcv and cmake using whatever package manager (e.g., apt-get,
+pacman) your linux distribution uses. Once you have unpacked the source in $CVBLOB, enter 
+in a console:
+
+	cd $CVBLOB
+	mkdir build
+	cd build
+	cmake ..
+	make
+	make install
+
+### Apple
+
+Use the [homebrew](http://mxcl.github.com/homebrew/) formula to install or follow the 
+linux example.
+
+    brew update
+    brew install $CVBLOB/homebrew/cvblob.rb
+
+### Useful CMake Options
+
+##### OpenCV Path
+
+You can tell CMake where OpenCV is installed:
+
+	cmake .. -DOpenCV_DIR=<path_to_OpenCV>
+
+#### Install Path
+
+To change the destination path for the installation, set "CMAKE_INSTALL_PREFIX" variable:
+
+	cmake .. -DCMAKE_INSTALL_PREFIX=<installation_path>
+
+## Documentation
+
+To build the documentation using [Doxygen](), just type:
+
+    make doc
+
+The documentation will get built in the doc folder in your build location. Just click
+on the index.html.
+
+## Example Usage
+
+blah ...
---
+++ test/test_tracking.cpp
@@ -0,0 +1,96 @@
+// Copyright (C) 2007 by Cristóbal Carnero Liñán
+// grendel.ccl@gmail.com
+//
+// This file is part of cvBlob.
+//
+// cvBlob is free software: you can redistribute it and/or modify
+// it under the terms of the Lesser GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// cvBlob is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// Lesser GNU General Public License for more details.
+//
+// You should have received a copy of the Lesser GNU General Public License
+// along with cvBlob.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#include <iostream>
+#include <string>
+
+
+#if (defined(_WIN32) || defined(__WIN32__) || defined(__TOS_WIN__) || defined(__WINDOWS__))
+    #include <opencv.h>
+    #include <highgui.h>
+#else
+    #include <opencv2/opencv.hpp>
+    #include <opencv2/highgui/highgui.hpp>
+#endif
+
+
+#include <cvblob.h>
+#include <cvRender.h>
+using namespace cvb;
+
+int main()
+{
+  CvTracks tracks;
+
+  cvNamedWindow("test_tracking", CV_WINDOW_AUTOSIZE);
+  
+  std::string file = "EnterExitCrossingPaths2front_blobs.mpeg";
+  CvCapture *capture=cvCreateFileCapture(file.c_str());
+  
+  if(capture <= 0){
+    std::cout << "Couldn't load " << file << std::endl;
+    exit(-1);
+  }
+
+  cvGrabFrame(capture);
+  IplImage *img = cvRetrieveFrame(capture);
+
+  IplImage *frame = cvCreateImage(cvGetSize(img), img->depth, img->nChannels);
+
+  while (cvGrabFrame(capture))
+  {
+    IplImage *img = cvRetrieveFrame(capture);
+
+    cvResetImageROI(frame);
+    cvConvertScale(img, frame, 1, 0);
+    cvThreshold(frame, frame, 100, 200, CV_THRESH_BINARY);
+
+    cvSetImageROI(frame, cvRect(0, 25, 383, 287));
+
+    IplImage *chB=cvCreateImage(cvGetSize(frame),8,1);
+    cvSplit(frame,chB,NULL,NULL,NULL);
+
+    IplImage *labelImg = cvCreateImage(cvGetSize(frame), IPL_DEPTH_LABEL, 1);
+
+    CvBlobs blobs;
+    unsigned int result = cvLabel(chB, labelImg, blobs);
+
+    cvFilterByArea(blobs, 500, 1000);
+
+    cvUpdateTracks(blobs, tracks, 5., 10);
+    //cvUpdateTracks(blobs, tracks, 10., 5);
+
+    cvRenderBlobs(labelImg, blobs, frame, frame, CV_BLOB_RENDER_CENTROID|CV_BLOB_RENDER_BOUNDING_BOX);
+    cvRenderTracks(tracks, frame, frame, CV_TRACK_RENDER_ID|CV_TRACK_RENDER_BOUNDING_BOX|CV_TRACK_RENDER_TO_LOG);
+
+    cvShowImage("test_tracking", frame);
+
+    cvReleaseImage(&chB);
+    cvReleaseImage(&labelImg);
+
+    if ((cvWaitKey(10)&0xff)==27)
+      break;
+  }
+
+  cvReleaseImage(&frame);
+
+  cvDestroyWindow("test_tracking");
+
+  return 0;
+}
